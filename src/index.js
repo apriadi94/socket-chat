beebb26b0f4dd1ec1399e2070c3db04cb8e5b09a
apriadi <dev.apriadi@gmail.com>
2021-06-03 17:15:29 +0700

    add env variable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const app = require('express')()
 const httpServer = require("http").createServer(app)
+const dotenv = require('dotenv')
+dotenv.config()
 const listener = require('./socket')
 const router = require('./routes')
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "*",
+    origin: process.env.APP_CORS,
   },
 });
 
 app.use('/', router)
 listener(io)
 
-httpServer.listen(8010)
+httpServer.listen(process.env.APP_PORT)
 
 module.exports = app;
