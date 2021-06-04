bb1131e6a8ab024e94c3bd873aabeddc78b6067a
apriadi <dev.apriadi@gmail.com>
2021-06-04 16:40:54 +0700

    menghubah metode test dari api ke socket.io

--- test/index.js
+++ test/index.js
@@ -1,23 +1,33 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../src/index');
+let io     = require('socket.io-client');
 
 chai.use(chaiHttp);
 chai.should();
 
+let expect = chai.expect;  
+
+var socketUrl = 'http://localhost:8010';
+
+var options = {  
+  transports: ['websocket'],
+  'force new connection': true
+};
 
 describe('Test', () => {
+  var client1;
 
-  describe('/GET /', () => {
-      it('it should GET status = 200', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  done();
-            });
+  describe('emit REQUEST_CONVERSATION', () => {
+    it('should listen of conversation and get array', function (done) {  
+      // Set up client1 connection
+      client1 = io.connect(socketUrl, options);
+  
+      client1.emit('REQUEST_CONVERSATION')
+      client1.on('CONVERSATION_SENT', function(msg){
+        expect(msg).to.be.a('array');
+        done();
       });
-  });
-
+    });
+  })
 });
