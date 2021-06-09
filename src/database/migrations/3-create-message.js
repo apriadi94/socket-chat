52ec169ae1fd83c44ec31996650925634b6e2d0a
apriadi <dev.apriadi@gmail.com>
2021-06-09 19:47:37 +0700

    membuat model dan migration Message

---
+++ src/database/migrations/3-create-message.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: { tableName: 'users' },
+          key: 'id',
+        }
+      },
+      roomId: {
+        type: Sequelize.INTEGER,
+        field: 'room_id',
+        references: {
+          model: { tableName: 'rooms' },
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Messages');
+  }
+};
---
+++ src/models/message.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Message.init({
+    userId: { 
+      type: DataTypes.INTEGER, 
+      field: 'user_id',
+      references: {
+        model: { tableName: 'users'},
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    roomId: { 
+      type: DataTypes.INTEGER, 
+      field: 'room_id',
+      references: {
+        model: { tableName: 'rooms' },
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    type: DataTypes.STRING,
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Message',
+  });
+  return Message;
+};
