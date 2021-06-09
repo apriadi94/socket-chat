4fe3640122789b1e7f416e8c0dc9be5423745915
apriadi <dev.apriadi@gmail.com>
2021-06-09 19:31:20 +0700

    make model and migration room

---
+++ src/database/migrations/20210609122843-create-room.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Rooms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        field: 'owner_id'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Rooms');
+  }
+};
