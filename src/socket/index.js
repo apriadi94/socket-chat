9fc72abd733aec2c0452baa830cee9946fd1d6a7
apriadi <dev.apriadi@gmail.com>
2021-06-09 10:55:01 +0700

    merapikan style code

--- src/socket/index.js
+++ src/socket/index.js
@@ -8,11 +8,11 @@ module.exports = io => {
         
         const listenersPath = resolve(`${__dirname}/listeners`);
         readdirSync(listenersPath)
-        .filter(file => file.slice(-3) === '.js' && file !== 'index.js')
-        .forEach(file => {
-            require(resolve(listenersPath, file))({ socket });
-        });
+            .filter(file => file.slice(-3) === '.js' && file !== 'index.js')
+            .forEach(file => {
+                require(resolve(listenersPath, file))({ socket });
+            });
         
         socket.on("disconnect", () => console.log("user disconnected"));
     })
-} 
+}
