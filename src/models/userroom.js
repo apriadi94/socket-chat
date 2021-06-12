4634623a9ccf6564c675067d83b5a160c0f5603f
apriadi <dev.apriadi@gmail.com>
2021-06-12 09:59:58 +0700

    membuat relasi userRoom dengan Room model

--- src/models/userroom.js
+++ src/models/userroom.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      UserRoom.hasOne(models.Room, { as: 'room', foreignKey: 'roomId' })
     }
   };
   UserRoom.init({
