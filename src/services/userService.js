c66122bfd10bfbb1e78958db047be3ff0802e9fd
apriadi <dev.apriadi@gmail.com>
2021-06-12 10:51:30 +0700

    menghilangkan console.log

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,10 @@
 const Models = require('../models')
-const logger = require('../utils/logger')
+const { logger } = require('../utils/logger')
 
 exports.addUser = async (id, body) => {
     const checkUser = await Models.User.count({ where: { id } })
     if(checkUser === 0){
         await Models.User.create(body).catch(err => {
-            console.log(err)
             logger.error(err)
         })
     }
